51b0a8762e0d43a74b438da316d6ab12b935b2cb
ZekaEu <zeka.surf@hotmail.com>
2026-02-21 17:08:24 -0300

    feat: real_real_final_final.c

--- real_final.c
+++ real_final.c
@@ -12,9 +12,9 @@ typedef struct s_client {
 }	t_client;
 
 t_client clients[1024];
-int maxfd = 0, globalid = 0;
-char sendbuff[400000], recvbuff[300000];
-fd_set writefds, readfds, currfds;
+int max_fd = 0, global_id = 0;
+char send_buff[400000], recv_buff[300000];
+fd_set write_fds, read_fds, curr_fds;
 
 void err(char *msg) {
 	if(msg)
@@ -26,10 +26,9 @@ void err(char *msg) {
 }
 
 void broadcast(int e) {
-	for(int fd = 0; fd <= maxfd; fd++)
-		if(FD_ISSET(fd, &writefds) && fd != e)
-			if(send(fd, sendbuff, strlen(sendbuff), 0) < 0)
-				err(NULL);
+	for(int fd = 0; fd <= max_fd; fd++)
+		if(FD_ISSET(fd, &write_fds) && fd != e)
+			if(send(fd, send_buff, strlen(send_buff), 0) < 0);
 }
 
 int main(int argc, char **argv) {
@@ -37,13 +36,16 @@ int main(int argc, char **argv) {
 		err("Wrong number of arguments");
 
 	struct sockaddr_in server;
-	int serverfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(serverfd < 0)
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(server_fd < 0)
 		err(NULL);
-	maxfd = serverfd;
+	max_fd = server_fd;
 
-	FD_ZERO(&currfds);
-	FD_SET(serverfd, &currfds);
+	FD_ZERO(&read_fds);
+	FD_ZERO(&write_fds);
+	FD_ZERO(&curr_fds);
+	FD_SET(server_fd, &curr_fds);
+
 	memset(&clients, 0, sizeof(clients));
 	memset(&server, 0, sizeof(server));
 
@@ -51,54 +53,60 @@ int main(int argc, char **argv) {
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	if(bind(serverfd, (const struct sockaddr *)&server, sizeof(server)) < 0) {
-        close(serverfd);
+	if(bind(server_fd, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        close(server_fd);
         err(NULL);
     }
-    if(listen(serverfd, 100) < 0) {
-        close(serverfd);
+    if(listen(server_fd, 100) < 0) {
+        close(server_fd);
         err(NULL);    
     }
 
 	while(1) {
-		writefds = readfds = currfds;
-		if(select(maxfd + 1, &readfds, &writefds, NULL, NULL) < 0)
-            continue;
+		write_fds = curr_fds;
+		read_fds = curr_fds;
+		if(select(max_fd + 1, &read_fds, &write_fds, NULL, NULL) < 0) {
+			close(server_fd);
+			FD_CLR(server_fd, &curr_fds);
+			err(NULL);
+		}
 
-		for(int fd = 0; fd <= maxfd; fd++) {
-			if(FD_ISSET(fd, &readfds)) {
-				if(fd == serverfd) {
+		for(int fd = 0; fd <= max_fd; fd++) {
+			if(FD_ISSET(fd, &read_fds)) {
+				if(fd == server_fd) {
                     socklen_t len = sizeof(server);
-					int clientfd = accept(fd, (struct sockaddr *)&server, &len);
-					if(clientfd < 0)
+					int client_fd = accept(fd, (struct sockaddr *)&server, &len);
+					if(client_fd < 0)
                         continue;
-					if(clientfd > maxfd)
-                        maxfd = clientfd;
-                    FD_SET(clientfd, &currfds);
-					clients[clientfd].id = globalid++;
-					sprintf(sendbuff, "server: client %d just arrived\n", clients[clientfd].id);
-					broadcast(clientfd);
-                    bzero(sendbuff, strlen(sendbuff));
+					if(client_fd > max_fd)
+                        max_fd = client_fd;
+                    FD_SET(client_fd, &curr_fds);
+					clients[client_fd].id = global_id++;
+					sprintf(send_buff, "server: client %d just arrived\n", clients[client_fd].id);
+					broadcast(client_fd);
+                    bzero(&send_buff, strlen(send_buff));
 					break;
 				} else {
-					ssize_t bufflen = recv(fd, recvbuff, sizeof(recvbuff), 0);
-					if(bufflen <= 0) {
-						bzero(&sendbuff, strlen(sendbuff));
-						sprintf(sendbuff, "server: client %d just left\n", clients[fd].id);					
+					ssize_t buff_len = recv(fd, recv_buff, sizeof(recv_buff), 0);
+					if(buff_len <= 0) {
+						bzero(&send_buff, strlen(send_buff));
+						sprintf(send_buff, "server: client %d just left\n", clients[fd].id);					
 						broadcast(fd);
-						FD_CLR(fd, &currfds);
+						FD_CLR(fd, &curr_fds);
 						close(fd);
                         bzero(&clients[fd].msg, strlen(clients[fd].msg));
 						break;
 					} else {
-						recvbuff[bufflen] = '\0';
-						for(int i = 0, j = strlen(clients[fd].msg); i < bufflen; i++, j++) {
-							clients[fd].msg[j] = recvbuff[i];
+						recv_buff[buff_len] = '\0';
+						int j = strlen(clients[fd].msg);
+						for(int i = 0; i < buff_len; i++, j++) {
+							clients[fd].msg[j] = recv_buff[i];
 							if(clients[fd].msg[j] == '\n') {
 								clients[fd].msg[j] = '\0';
-								sprintf(sendbuff, "client %d: %s\n", clients[fd].id, clients[fd].msg);
+								sprintf(send_buff, "client %d: %s\n", clients[fd].id, clients[fd].msg);
 								broadcast(fd);
-								bzero(clients[fd].msg, strlen(clients[fd].msg));
+								bzero(&send_buff, strlen(send_buff));
+								bzero(&clients[fd].msg, strlen(clients[fd].msg));
 								j = -1;
 							}
 						}
